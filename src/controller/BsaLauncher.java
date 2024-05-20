21219a4aad9f8e14d309d9aac296f9fa7e1ec128
opesnoir <goodsurroundsme@outlook.com>
2024-05-20 15:29:40 +0200

    feat toon resultaten + opmaak

--- src/controller/BsaLauncher.java
+++ src/controller/BsaLauncher.java
@@ -10,20 +10,25 @@ public class BsaLauncher {
     public static void main(String[] args) {
         final double CIJFER_ONDERGRENS = 1.0;
         final double CIJFER_BOVENGRENS = 10.0;
+        final double CESUUR = 5.5;
+        final int TOTAAL_TE_BEHALEN_STUDIEPUNTEN = 28;
+        int sumBehaaldeStudiepunten = 0;
 
         Vak[] vakken = new Vak[7];
-        vakken[0] = new Vak("Project Fasten Your Seatbelts", 12, 5.5);
-        vakken[1] = new Vak("Programming", 3, 5.5);
-        vakken[2] = new Vak("Databases", 3, 5.5);
-        vakken[3] = new Vak("Personal Skills", 2, 5.5);
-        vakken[4] = new Vak("Project Skills", 2, 5.5);
-        vakken[5] = new Vak("OOP", 3, 5.5);
-        vakken[6] = new Vak("User Interaction", 3, 5.5);
+        vakken[0] = new Vak("Project Fasten Your Seatbelts", 12, CESUUR);
+        vakken[1] = new Vak("Programming", 3, CESUUR);
+        vakken[2] = new Vak("Databases", 3, CESUUR);
+        vakken[3] = new Vak("Personal Skills", 2, CESUUR);
+        vakken[4] = new Vak("Project Skills", 2, CESUUR);
+        vakken[5] = new Vak("OOP", 3, CESUUR);
+        vakken[6] = new Vak("User Interaction", 3, CESUUR);
 
         Score[] scores = new Score[7];
 
+        // scanner declareren om input op te halen
         Scanner input = new Scanner(System.in);
         System.out.println("Voer de behaalde cijfers in:");
+        // for loop om input op te halen en op te slaan
         for (int i = 0; i < scores.length; i++) {
             double behaaldeCijfer;
             do {
@@ -32,7 +37,18 @@ public class BsaLauncher {
                 input.nextLine(); // enter opvangen die blijft hangen, zodat je double cijfers kunt invoeren zonder foutmelding
             } while (behaaldeCijfer < CIJFER_ONDERGRENS || behaaldeCijfer > CIJFER_BOVENGRENS);
             scores[i] = new Score(vakken[i], behaaldeCijfer); // if statement is niet nodig, de loop eindigt pas als het cijfer tussen de 1,0 en 10,0 ligt
+
+            // som van alle studiepunten samen
+            sumBehaaldeStudiepunten += scores[i].getBehaaldePunten();
         }
+        // toon resultaten
+        for (int i = 0; i < vakken.length; i++) {
+            System.out.printf("Vak/project: %-30s Cijfer: %-6.2f Behaalde punten: %-5d\n", vakken[i].getName(), scores[i].getCijfer(), scores[i].getBehaaldePunten());
+
+        }
+        System.out.printf("Totaal behaalde studiepunten zijn: %d/%d", sumBehaaldeStudiepunten, TOTAAL_TE_BEHALEN_STUDIEPUNTEN);
+
+
 
     }
 }
